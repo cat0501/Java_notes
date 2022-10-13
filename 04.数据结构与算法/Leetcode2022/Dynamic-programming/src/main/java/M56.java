e7712832d80af5790e231a9894eb063286467d9c
Lemonade19 <17782975312@163.com>
2022-10-13 17:41:06 +0800

    leetcode刷题

--- 04.数据结构与算法/Leetcode2022/Dynamic-programming/src/main/java/M56.java
+++ 04.数据结构与算法/Leetcode2022/Dynamic-programming/src/main/java/M56.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @author Lemonade
@@ -8,10 +9,56 @@ import java.util.Arrays;
  */
 
 public class M56 {
+
+    //public static void main(String args[]){
+    //    int intervals[][] = {{2,6},{1,3},{8,10},{15,18}};
+    //
+    //    System.out.println("排序前");
+    //    for (int i = 0; i < intervals.length; i++){
+    //        for (int j = 0; j < 2; j++){
+    //            System.out.print(intervals[i][j]+" ");
+    //        }
+    //        System.out.println();
+    //    }
+    //
+    //    //重写Arrays.sort()方法
+    //    Arrays.sort(intervals, new Comparator<int[]>() {
+    //        @Override
+    //        public int compare(int[] o1, int[] o2) {
+    //            //这里根据第一列进行排序
+    //            return o1[0] - o2[0];
+    //        }
+    //    });
+    //
+    //    System.out.println("排序后");
+    //    for (int i = 0; i < intervals.length; i++){
+    //        for (int j = 0; j < 2; j++){
+    //            System.out.print(intervals[i][j] +" ");
+    //        }
+    //        System.out.println();
+    //    }
+    //}
+
+
+
     public static void main(String[] args) {
         int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] intervals2 = {{1,4},{4,5}};
+        int[][] intervals3 = {{1,3}};
+        int[][] intervals4 = {{1,4},{0,4}};
+        int[][] intervals5 = {{1,4},{0,1}};
+        // 118 / 170 个通过的测试用例
+        int[][] intervals6 = {{1,4},{0,0}};
+        int[][] intervals7 = {{4,5},{1,4},{0,1}};
 
-        System.out.println(Arrays.deepToString(new Solution56().merge(intervals)));
+
+        //System.out.println(Arrays.deepToString(new Solution56().merge(intervals)));
+        //System.out.println(Arrays.deepToString(new Solution56().merge(intervals2)));
+        //System.out.println(Arrays.deepToString(new Solution56().merge(intervals3)));
+        //System.out.println(Arrays.deepToString(new Solution56().merge(intervals4)));
+        //System.out.println(Arrays.deepToString(new Solution56().merge(intervals5)));
+        //System.out.println(Arrays.deepToString(new Solution56().merge(intervals6)));
+        System.out.println(Arrays.deepToString(new Solution56().merge(intervals7)));
     }
 }
 
@@ -23,26 +70,37 @@ public class M56 {
 
 class Solution56 {
     public int[][] merge(int[][] intervals) {
-        ArrayList<int[]> arrayList = new ArrayList<>();
+        //重写Arrays.sort()方法
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                //这里根据第一列进行排序
+                return o1[0] - o2[0];
+            }
+        });
+
+        if (intervals.length == 1){
+            return intervals;
+        }
+
+        int[][] mergeDp = mergeDp(intervals);
+
+        while (check(mergeDp)){
+            mergeDp = mergeDp(mergeDp);
+        }
+        return mergeDp;
+    }
+
+    // 是否不可再合并
+    public boolean check(int[][] intervals){
         for (int i = 0; i < intervals.length - 1; i++) {
             int[] left = intervals[i];
             int[] right = intervals[i + 1];
-
-            int[] arr = new int[0];
-            if (left[left.length - 1] > right[0]){
-                int leftN = left[0];
-                int rightN = right[right.length - 1];
-                arr = new int[]{leftN, rightN};
-                //原数组未改变，死循环
-                //--i;
-            } else {
-                arr = intervals[i];
+            if (left[left.length - 1] >= right[0]){
+                return true;
             }
-            arrayList.add(arr);
         }
-
-        arrayList.add(intervals[intervals.length - 1]);
-        return arrayList.toArray(new int[0][]);
+        return false;
     }
 
     public int[][] mergeDp(int[][] intervals) {
@@ -53,19 +111,35 @@ class Solution56 {
             int[] right = intervals[i + 1];
 
             int[] arr = new int[0];
-            if (left[left.length - 1] > right[0]){
-                int leftN = left[0];
-                int rightN = right[right.length - 1];
+            // 一共几种情况？考虑全面
+            // 情况1：如果  [1,3],[2,6]，那么[1, 6]
+            // 情况2：如果  [1,4],[0,4]，那么[0, 4]
+            if (left[left.length - 1] >= right[0]){
+                int leftN;
+                int rightN;
+                if (left[0] >= right[0]){
+                    leftN = right[0];
+                } else {
+                    leftN = left[0];
+                }
+                if (left[left.length - 1] >= right[right.length - 1]){
+                    rightN = left[right.length - 1];
+                } else {
+                    rightN = right[right.length - 1];
+                }
                 arr = new int[]{leftN, rightN};
                 //原数组未改变，死循环
                 //--i;
+                ++i;
             } else {
                 arr = intervals[i];
             }
             arrayList.add(arr);
         }
-
-        arrayList.add(intervals[intervals.length - 1]);
+        int[] ints = arrayList.get(arrayList.size() - 1);
+        if (ints[ints.length - 1] < intervals[intervals.length - 1][intervals[0].length - 1]){
+            arrayList.add(intervals[intervals.length - 1]);
+        }
         return arrayList.toArray(new int[0][]);
     }
 }
---
+++ 04.数据结构与算法/Leetcode2022/Dynamic-programming/src/main/java/M769.java
@@ -0,0 +1,49 @@
+/**
+ * @author Lemonade
+ * @description
+ * @updateTime 2022/10/13 16:46
+ */
+
+
+public class M769 {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,1,0};
+        int[] arr2 = {1,0,2,3,4};
+        System.out.println(new Solution769().maxChunksToSorted(arr));
+        System.out.println(new Solution769().maxChunksToSorted(arr2));
+    }
+
+}
+
+class Solution769 {
+    public int maxChunksToSorted(int[] arr) {
+        int count = 0;
+        int vSum = 0;
+        int iSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            vSum += arr[i];
+            iSum += i;
+            if (vSum == iSum){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+
+
+//输入: arr = [4,3,2,1,0]
+//输出: 1
+//解释:
+//将数组分成2块或者更多块，都无法得到所需的结果。
+//例如，分成 [4, 3], [2, 1, 0] 的结果是 [3, 4, 0, 1, 2]，这不是有序的数组。
+
+
+//输入: arr = [1,0,2,3,4]
+//输出: 4
+//解释:
+//我们可以把它分成两块，例如 [1, 0], [2, 3, 4]。
+//然而，分成 [1, 0], [2], [3], [4] 可以得到最多的块数。
