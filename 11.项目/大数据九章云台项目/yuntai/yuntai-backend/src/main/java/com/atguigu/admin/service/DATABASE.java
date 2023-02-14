af48f4f7532004aab8b4acc39d018ffc7f92dfec
Lemonade19 <17782975312@163.com>
2023-02-14 10:24:54 +0800

    update

--- 11.项目/大数据九章云台项目/yuntai/yuntai-backend/src/main/java/com/atguigu/admin/service/DATABASE.java
+++ 11.项目/大数据九章云台项目/yuntai/yuntai-backend/src/main/java/com/atguigu/admin/service/DATABASE.java
@@ -2,7 +2,7 @@ package com.atguigu.admin.service;
 
 public class DATABASE {
     public static String DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static String URL = "jdbc:mysql://localhost:3306/yuntai_admin?useSSL=false&characterEncoding=utf8";
+    public static String URL = "jdbc:mysql://49.232.132.201:3306/yuntai_admin?useSSL=false&characterEncoding=utf8";
     public static String USERNAME = "root";
     public static String PASSWORD = "123456";
 }
--- 11.项目/大数据九章云台项目/yuntai/yuntai-backend/src/main/java/com/atguigu/statistic/service/DATABASE.java
+++ 11.项目/大数据九章云台项目/yuntai/yuntai-backend/src/main/java/com/atguigu/statistic/service/DATABASE.java
@@ -1,11 +1,11 @@
 package com.atguigu.statistic.service;
 
 public class DATABASE {
-    public static String MYSQL_URL = "jdbc:mysql://localhost:3306/gmall_report?useSSL=false";
+    public static String MYSQL_URL = "jdbc:mysql://49.232.132.201:3306/gmall_report?useSSL=false";
     public static String MYSQL_USERNAME = "root";
-    public static String MYSQL_PASSWORD = "root";
+    public static String MYSQL_PASSWORD = "123456";
     public static String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    public static String CLICKHOUSE_URL = "jdbc:clickhouse://localhost:8123/default";
+    public static String CLICKHOUSE_URL = "jdbc:clickhouse://49.232.132.201:8123/default";
     public static String CLICKHOUSE_DRIVER = "com.clickhouse.jdbc.ClickHouseDriver";
 }
