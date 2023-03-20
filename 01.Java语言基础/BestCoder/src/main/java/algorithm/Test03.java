7909dcdda311493bfa95add4c2cff20724384601
Lemonade19 <17782975312@163.com>
2023-03-20 18:07:01 +0800

    update xxx

---
+++ 01.Java语言基础/BestCoder/src/main/java/algorithm/Test03.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+/**
+ * @author Lemonade
+ * @description
+ * @updateTime 2023/3/20 14:21
+ */
+public class Test03 {
+    public static void main(String[] args) {
+        String sb = "aaa" +
+                System.lineSeparator() +
+                "bbb" +
+                System.lineSeparator() +
+                "ccc";
+        System.out.println(sb + ";");
+        /**
+         aaa
+         bbb
+         ccc
+         */
+    }
+}
