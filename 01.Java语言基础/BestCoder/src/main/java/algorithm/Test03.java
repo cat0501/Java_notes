ff9117a15b3181afe02d0e80f8dae8e6d89fb21b
Lemonade19 <17782975312@163.com>
2023-03-29 11:33:16 +0800

    update

--- 01.Java语言基础/BestCoder/src/main/java/algorithm/Test03.java
+++ 01.Java语言基础/BestCoder/src/main/java/algorithm/Test03.java
@@ -12,11 +12,34 @@ public class Test03 {
                 "bbb" +
                 System.lineSeparator() +
                 "ccc";
-        System.out.println(sb + ";");
+        //System.out.println(sb + ";");
+
+        String a = new String("adsadsasw");
+        // Common
+        /**
+         遗礼可以拿，主要是为了没人打你也能吃桃刷技能
+         */
+        a = a.substring(0, a.length() - 1);
+        System.out.println(a);
         /**
          aaa
          bbb
          ccc
          */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 }
