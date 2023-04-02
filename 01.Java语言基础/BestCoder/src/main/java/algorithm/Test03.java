b7f726429c5694e87c398e3ce8ad07c8a16b3a86
Lemonade19 <17782975312@163.com>
2023-04-02 22:05:54 +0800

    update

--- 01.Java语言基础/BestCoder/src/main/java/algorithm/Test03.java
+++ 01.Java语言基础/BestCoder/src/main/java/algorithm/Test03.java
@@ -1,5 +1,8 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author Lemonade
  * @description
@@ -7,31 +10,45 @@ package algorithm;
  */
 public class Test03 {
     public static void main(String[] args) {
-        // System.lineSeparator();
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("id");
+        list.add("vin");
+
+        if (list.contains("id")){
+            System.out.println("yes");
+        }
+
+        String[] str = {"2","2","200","xian2","huizhou2"};
+        System.out.println(Arrays.toString(str));
+        System.out.println(Arrays.toString(str).replace(","," "));
+        System.out.println(Arrays.toString(str).replace("[","").replace("]",""));
+
+        //// System.lineSeparator();
+        ////
+        //String sb = "aaa" +
+        //        System.lineSeparator() +
+        //        "bbb" +
+        //        System.lineSeparator() +
+        //        "ccc";
+        ////System.out.println(sb + ";");
         //
-        String sb = "aaa" +
-                System.lineSeparator() +
-                "bbb" +
-                System.lineSeparator() +
-                "ccc";
-        //System.out.println(sb + ";");
-
-        // Common
-        /**
-         When things are uncertain, you can ask the spring breeze.
-        下班打印英语真题
-
-         */
-
-        // String API
-        String a = new String("adsadsasw");
-        a = a.substring(0, a.length() - 1);
-        System.out.println(a);
-        /**
-         aaa
-         bbb
-         ccc
-         */
+        //// Common
+        ///**
+        // When things are uncertain, you can ask the spring breeze.
+        //下班打印英语真题
+        //
+        // */
+        //
+        //// String API
+        //String a = new String("adsadsasw");
+        //a = a.substring(0, a.length() - 1);
+        //System.out.println(a);
+        ///**
+        // aaa
+        // bbb
+        // ccc
+        // */
 
 
 
--- 03.数据库/00_SQL.md
+++ 03.数据库/00_SQL.md
@@ -10,3 +10,148 @@ JDBC 驱动程序将 JDBC API 转换为特定数据库的本机协议
 
 
 
+```java
+// 获取连接
+conn = DriverManager.getConnection(url, username, pw);
+```
+
+
+
+
+
+IDEA 将一段代码抽取为一个方法
+
+
+
+
+
+
+
+**hive 使用 `load` 导入数据时是否可以指定分隔符？**
+
+hive load 数据只是单纯的把文件拷贝到 `hdfs` 的相应目录下面，并不作格式检查和解析。
+
+只有在查询数据的时候，才会根据创建表时定义的序列化方式解析数据。建表的时候可以指定分隔符。
+
+
+
+
+
+```java
+public static ArrayList<String> readCsvByBufferedReader(String filePath) {
+    File csv = new File(filePath);
+    csv.setReadable(true);
+    csv.setWritable(true);
+    InputStreamReader isr = null;
+    BufferedReader br = null;
+    try {
+        isr = new InputStreamReader(new FileInputStream(csv), "UTF-8");
+        br = new BufferedReader(isr);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    String line = "";
+    ArrayList<String> records = new ArrayList<>();
+    try {
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+            records.add(line);
+        }
+        System.out.println("csv表格读取行数：" + records.size());
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    System.out.println(records);
+    return records;
+}
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
